662650bb5c125b0ad9516513275b180f3ac6af00
Christopher Lazar <chrislazar3@aol.com>
2024-05-20 22:06:28 -0700

    added comment for cicd.

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -21,7 +21,7 @@ module.exports = app => {
   // Delete a Tutorial with id
   router.delete("/:id", tutorials.delete);
 
-  // Delete all Tutorials
+  // Delete all Tutorials comment test
   router.delete("/", tutorials.deleteAll);
 
   app.use('/api/tutorials', router);
