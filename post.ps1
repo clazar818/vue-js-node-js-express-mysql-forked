c775b59808c6d1100cde9718b06e4f8b299c3a92
Christopher Lazar <chrislazar3@aol.com>
2024-05-20 20:35:36 -0700

    feat: Added post ps1 demo script.

---
+++ post.ps1
@@ -0,0 +1,14 @@
+# Define the URL
+$url = "http://main-lb-263015034.us-east-1.elb.amazonaws.com/api/tutorials"
+
+# Create the JSON body
+$body = @{
+    title       = "TestData"
+    description = "TestData"
+} | ConvertTo-Json
+
+# Make the POST request
+$response = Invoke-RestMethod -Uri $url -Method Post -ContentType "application/json" -Body $body
+
+# Output the response
+$response
